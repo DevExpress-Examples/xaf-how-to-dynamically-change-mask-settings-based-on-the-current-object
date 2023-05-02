ef8338a06b3cbb5fc2e42cc5874d362a3ad47730
andrey <andrey.kozhevnikov@devexpress.com>
2023-05-02 16:26:45 +0400

    blazor work

--- CS/XPO/DynamicMask/DynamicMask.Blazor.Server/Controllers/WebChangeMaskController.cs
+++ CS/XPO/DynamicMask/DynamicMask.Blazor.Server/Controllers/WebChangeMaskController.cs
@@ -1,4 +1,5 @@
-﻿using DevExpress.ExpressApp.Blazor.Editors;
+﻿using DevExpress.ExpressApp.Blazor.Components.Models;
+using DevExpress.ExpressApp.Blazor.Editors;
 using DevExpress.ExpressApp.Blazor.Editors.Adapters;
 using DynamicMask.Module.BusinessObjects;
 using DynamicMask.Module.Controllers;
@@ -7,27 +8,29 @@ namespace ChangeEditMask.Module.Web.Controllers {
     public class BlazorChangeMaskController : ChangeMaskControllerBase {
         protected override void SetControlMaskSettings(DevExpress.ExpressApp.Editors.PropertyEditor propertyEditor, EditMask mask) {
             if (propertyEditor is StringPropertyEditor stringEditor) {
-
-                //    ASPxTextBox textEdit = ((ASPxStringPropertyEditor)propertyEditor).Editor as ASPxTextBox;
-                //var t = stringEditor.Control as DxTextBoxAdapter;
-                //var t2 = t.ComponentModel;
-                //    if (textEdit != null) {
+                var adapter = stringEditor.Control as DxMaskedInputAdapter;
+                var componentModel = adapter?.ComponentModel;
+                if (componentModel == null) {
+                    return;
+                }
                 switch (mask) {
                     case EditMask.Date:
-                        stringEditor.EditMask = "MM/dd/yyyy";
+                        componentModel.MaskMode = DevExpress.Blazor.MaskMode.DateTime;
+                        componentModel.Mask = "MM/dd/yyyy";
                         break;
                     case EditMask.Time:
-                        stringEditor.EditMask = "hh:mm tt";
+                        componentModel.MaskMode = DevExpress.Blazor.MaskMode.DateTime;
+                        componentModel.Mask = "hh:mm tt";
                         break;
                     case EditMask.Numeric:
-                        //   stringEditor.EditMask = "(000) 000-0000";
-                        stringEditor.Model.EditMask = "(000) 000-0000";
-                        View.Refresh();
+                        componentModel.MaskMode = DevExpress.Blazor.MaskMode.Numeric;
+                        componentModel.Mask = "(000) 000-0000";
                         break;
                     case EditMask.String:
+                        componentModel.MaskMode = DevExpress.Blazor.MaskMode.Auto;
+                        componentModel.Mask = "";
                         break;
                 }
-                //    }
             }
         }
     }
