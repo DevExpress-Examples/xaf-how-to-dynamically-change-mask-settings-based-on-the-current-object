e1887a0ae5e5cbfe940fa8cbfe10fc5b9bc2f0a9
andrey <andrey.kozhevnikov@devexpress.com>
2023-05-02 15:45:56 +0400

    blazor (not work)

---
+++ CS/XPO/DynamicMask/DynamicMask.Blazor.Server/Controllers/WebChangeMaskController.cs
@@ -0,0 +1,29 @@
+﻿using DevExpress.ExpressApp.Blazor.Editors;
+using DynamicMask.Module.BusinessObjects;
+using DynamicMask.Module.Controllers;
+
+namespace ChangeEditMask.Module.Web.Controllers {
+    public class BlazorChangeMaskController : ChangeMaskControllerBase {
+        protected override void SetControlMaskSettings(DevExpress.ExpressApp.Editors.PropertyEditor propertyEditor, EditMask mask) {
+            if (propertyEditor is StringPropertyEditor stringEditor) {
+
+                //    ASPxTextBox textEdit = ((ASPxStringPropertyEditor)propertyEditor).Editor as ASPxTextBox;
+                //    if (textEdit != null) {
+                switch (mask) {
+                    case EditMask.Date:
+                        stringEditor.EditMask = "MM/dd/yyyy";
+                        break;
+                    case EditMask.Time:
+                        stringEditor.EditMask = "hh:mm tt";
+                        break;
+                    case EditMask.Numeric:
+                        stringEditor.EditMask = "(000) 000-0000";
+                        break;
+                    case EditMask.String:
+                        break;
+                }
+                //    }
+            }
+        }
+    }
+}
--- CS/XPO/DynamicMask/DynamicMask.Win/Controllers/WinChangeMaskController.cs
+++ CS/XPO/DynamicMask/DynamicMask.Win/Controllers/WinChangeMaskController.cs
@@ -1,31 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using DynamicMask.Module.Controllers;
+﻿using DevExpress.ExpressApp.Editors;
 using DevExpress.ExpressApp.Win.Editors;
 using DevExpress.XtraEditors;
+using DevExpress.XtraEditors.Mask;
+using DynamicMask.Module.BusinessObjects;
+using DynamicMask.Module.Controllers;
 
 namespace DynamicMask.Module.Win.Controllers {
     public class WinChangeMaskController : ChangeMaskControllerBase {
-        protected override void SetControlMaskSettings(DevExpress.ExpressApp.Editors.PropertyEditor propertyEditor, BusinessObjects.EditMask mask) {
+        protected override void SetControlMaskSettings(PropertyEditor propertyEditor, EditMask mask) {
             if (propertyEditor is StringPropertyEditor) {
                 TextEdit textEdit = ((StringPropertyEditor)propertyEditor).Control;
                 switch (mask) {
-                    case BusinessObjects.EditMask.Date:
-                        textEdit.Properties.Mask.MaskType = DevExpress.XtraEditors.Mask.MaskType.DateTime;
+                    case EditMask.Date:
+                        textEdit.Properties.Mask.MaskType = MaskType.DateTime;
                         textEdit.Properties.Mask.EditMask = "d";
                         break;
-                    case BusinessObjects.EditMask.Time:
-                        textEdit.Properties.Mask.MaskType = DevExpress.XtraEditors.Mask.MaskType.DateTime;
+                    case EditMask.Time:
+                        textEdit.Properties.Mask.MaskType = MaskType.DateTime;
                         textEdit.Properties.Mask.EditMask = "t";
                         break;
-                    case BusinessObjects.EditMask.Numeric:
-                        textEdit.Properties.Mask.MaskType = DevExpress.XtraEditors.Mask.MaskType.Numeric;
+                    case EditMask.Numeric:
+                        textEdit.Properties.Mask.MaskType = MaskType.Numeric;
                         textEdit.Properties.Mask.EditMask = "d";
                         break;
-                    case BusinessObjects.EditMask.String:
-                        textEdit.Properties.Mask.MaskType = DevExpress.XtraEditors.Mask.MaskType.None;
+                    case EditMask.String:
+                        textEdit.Properties.Mask.MaskType = MaskType.None;
                         textEdit.Properties.Mask.EditMask = "";
                         break;
                 }
